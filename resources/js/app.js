fd03a3f9e2b79f9119eadd5858736ccc5ea7f251
Andrei Goncharenko <andrei.h@seranking.com>
2025-11-03 11:23:54 +0100

    Set step 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
-// resources/js/app.js
 import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Базовый JavaScript
-console.log('Laravel app loaded');
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,10 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    // Явно указываем базовый URL
-    base: process.env.NODE_ENV === 'production' 
-        ? '/build/' 
-        : '/',
 });
