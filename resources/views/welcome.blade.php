2a5eef24c1554f598be788a91264d393c72e4d3b
Andrei Goncharenko <andrei.h@seranking.com>
2025-11-08 02:42:33 +0100

    fix hexlet lint

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-                    {{ __('layout.title') }}
+                   {{ __('Привет от Хекслета!') }}
                 </h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                     {{ __('layout.home_content_2') }}        </p>
