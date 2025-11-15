2a8d7823dde7ef65b4cc987d51fd6c68ee9ee2bc
Andrei Goncharenko <andrei.h@seranking.com>
2025-11-15 18:00:20 +0100

    fix bugs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,10 @@
+import ujs from '@rails/ujs'
 import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import Alpine from 'alpinejs';
 
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Alpine = Alpine;
 
-Alpine.start();
-
-import ujs from '@rails/ujs'
 ujs.start()
+Alpine.start();
