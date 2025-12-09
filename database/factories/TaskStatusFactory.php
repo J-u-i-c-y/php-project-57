31b3430779b64ca4a6f55783fb944ce1dac61ddf
Andrei Goncharenko <andrei.h@seranking.com>
2025-12-09 17:43:02 +0100

    fix bugs

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -2,17 +2,23 @@
 
 namespace Database\Factories;
 
-use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TaskStatus>
+ */
 class TaskStatusFactory extends Factory
 {
-    protected $model = TaskStatus::class;
-
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
+            'name' => fake()->unique()->word(),
         ];
     }
 }
