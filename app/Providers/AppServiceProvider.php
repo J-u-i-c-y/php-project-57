467996502f64bca48d6bb09846e3eaa4af9bca6c
Andrei Goncharenko <andrei.h@seranking.com>
2025-12-09 16:14:48 +0100

    fix bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,34 +2,24 @@
 
 namespace App\Providers;
 
-use App\Models\Label;
-use App\Models\Task;
-use App\Models\TaskStatus;
-use App\Policies\LabelPolicy;
-use App\Policies\TaskPolicy;
-use App\Policies\TaskStatusPolicy;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
-class AuthServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
+     * Register any application services.
      */
-    protected $policies = [
-        TaskStatus::class => TaskStatusPolicy::class,
-        Task::class => TaskPolicy::class,
-        Label::class => LabelPolicy::class,
-    ];
+    public function register(): void
+    {
+        //
+    }
 
     /**
-     * Register any authentication / authorization services.
+     * Bootstrap any application services.
      */
     public function boot(): void
     {
-        $this->registerPolicies();
         app()->setLocale('ru');
 
         if (config('app.env') != 'local') {
