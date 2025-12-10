345d80468d43e624dd74f46c72a1f086bed77dbc
Andrei Goncharenko <andrei.h@seranking.com>
2025-12-10 15:45:09 +0100

    fix bugs

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,7 +65,7 @@ class User extends Authenticatable
 
     /**
      * Задачи, созданные пользователем.
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function createdTasks()
@@ -75,7 +75,7 @@ class User extends Authenticatable
 
     /**
      * Задачи, назначенные пользователю.
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function assignedTasks()
