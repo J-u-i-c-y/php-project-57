bb214a541b2d9b8a68cd6bdad094add6be4761f0
Andrei Goncharenko <andrei.h@seranking.com>
2025-12-19 14:14:24 +0100

    fix bugs

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -8,6 +8,11 @@ use App\Models\Label;
 
 class LabelController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Label::class);
+    }
+    
     public function index()
     {
         $labels = Label::paginate();
@@ -21,14 +26,11 @@ class LabelController extends Controller
 
     public function create()
     {
-        $this->authorize('create', Label::class);
         return view('labels.create');
     }
 
     public function store(LabelStoreRequest $request)
     {
-        $this->authorize('create', Label::class);
-
         Label::create($request->validated());
         flash(__('controllers.label_create'))->success();
         return redirect()->route('labels.index');
@@ -36,14 +38,11 @@ class LabelController extends Controller
 
     public function edit(Label $label)
     {
-        $this->authorize('update', $label);
         return view('labels.edit', compact('label'));
     }
 
     public function update(LabelUpdateRequest $request, Label $label)
     {
-        $this->authorize('update', $label);
-
         $label->update($request->validated());
         flash(__('controllers.label_update'))->success();
         return redirect()->route('labels.index');
